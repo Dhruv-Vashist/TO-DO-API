f9d72e78d5737f3d4f107f73afc76049ad3f0cea
boundlessomnipotent <dhruv.03v@gmail.com>
2026-01-31 13:29:01 +0530

    basic crud operation

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , From
+from typing import Annotated
+from schema.User import User
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/signin")
+def signin(data: Annotated[User, From()]):
+    return data
+
+@app.post("/signup")
+async def create_user():
+    return {"status": "User created"}
+
+@app.get("/tasks")
+async def get_tasks():
+    return {"tasks": []}
+
+@app.post("/tasks")
+async def create_task():
+    return {"status": "Task created"}
+
+@app.put("/tasks/{task_id}")
+async def update_task(task_id: int):
+    return {"status": f"Task {task_id} updated"}
+
+@app.delete("/tasks/{task_id}")
+async def delete_task(task_id: int):
+    return {"status": f"Task {task_id} deleted"}
